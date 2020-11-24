52ea6324abbf94804e796de6c2cbd99f3cfb84f1
Adeline <adeline.servan@gmail.com>
2020-11-24 10:12:37 +0400

    affichage lettre de l'alphabet

---
+++ player/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<title>Jeu du pendu</title>
+</head>
+<body>
+	<main class="container">
+
+		<section class="row">
+
+			<div class=" col-8 card border-0 mx-auto">
+				<div class="card-body">
+					Tentez de deviner le mot secret en entrant des lettres une par une au clavier. Ne gaspillez pas vos coups, car si trop de vos choix sont erronés vous tuerez le pendu et vous perdrez la partie.
+					<div class="container">
+						<div class="row">
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">A</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">B</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">C</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">D</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">E</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">F</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">G</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">H</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">I</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">J</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">K</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">L</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">M</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">N</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">O</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">P</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">Q</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">R</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">S</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">T</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">U</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">V</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">W</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">X</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">Y</button>
+							<button type="button" class="btn btn-info alpha mb-2 mr-2">Z</button>
+						</div>
+					</div>
+					
+					_ _ _ _ _ _ _ _ 
+
+
+				</div>
+			</div>
+			
+		</section>
+		
+	</main>
+	    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
